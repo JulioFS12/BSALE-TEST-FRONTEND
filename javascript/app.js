14fc8c93e2b91b14b7f21f4661b9948ea97e1792
Julio Flores <juliocesarflores12@gmail.com>
2021-03-14 12:47:33 -0400

    fix: docs intrusive

--- javascript/app.js
+++ javascript/app.js
@@ -1,23 +1,24 @@
-const tasksView = document.getElementById('products-section');
+const productView = document.getElementById('products-section');
+const categoryView = document.getElementById('cars');
+const form = document.getElementById('form');
+var category = -1;
 
-const showDocument = ({ data }) => {
+const showDocument = ({data}) => {
 
-    tasksView.innerHTML = '';
+    productView.innerHTML = '';
 
-    if(data.length <= 0){
-        return tasksView.innerHTML += `NO RESULTS`
+    if (data.length <= 0) {
+        return productView.innerHTML += `<h4>NO RESULTS</h4>`
     }
 
     for (let i = 0; i < 20; i++) {
 
-        const { url_image = '', name, price } = data[i];
-
-        tasksView.innerHTML += `
+        productView.innerHTML += `
         <div class="main-container__card">
-            <img src="${!url_image?`../assets/empty.jpeg`:url_image}" class="desing" alt="product">
+            <img src="${!data[i].url_image > 5? `../assets/empty.jpeg` : data[i].url_image}" class="desing" alt="product">
                 <div class="main-container__card__info">
-                    <p class="">${!name?`empty`:name}</p>
-                    <p class="money">$ ${!price?`empty`:price}</p>
+                    <p class="">${!data[i].name ? `empty` : data[i].name}</p>
+                    <p class="money">$ ${!data[i].price ? `empty` : data[i].price}</p>
                     <button class="products-section__button">
                         <i class="fas fa-plus-circle"></i>
                     </button>
@@ -27,35 +28,37 @@ const showDocument = ({ data }) => {
 
 }
 
-const getAll = () => {
-    tasksView.innerHTML = `WAIT A MINUTE...!`;
-    fecthApi('all', '');
+const showComboBox = ({data}) => {
+
+    for (let i = 0; i < data.length; i++) {
+        const { id, name } = data[i];
+        categoryView.innerHTML += `<option value="${id}">${name}</option>`;        
+    }
+
 }
 
-const getByName = (e) => {
-    e.preventDefault();
-    tasksView.innerHTML = `WAIT A MINUTE...!`;
-    const key = document.getElementById('name').value;
-
-    fecthApi('byName', key);
-}
-
-const fecthApi = (option, key) => {
+const fecthApi = (option, key, second_key) => {
 
     const options = {
         method: "GET"
     };
 
     switch (option) {
-        case 'all':
+        case 'allProducts':
 
             fetch(`http://localhost:5000/products/all`, options)
                 .then(response => response.json())
                 .then(data => {
                     showDocument(data)
                 })
-                .catch(err => {
-                    console.log(err);
+            break;
+
+        case 'categories':
+
+            fetch(`http://localhost:5000/category/all`, options)
+                .then(response => response.json())
+                .then(data => {
+                    showComboBox(data);
                 })
             break;
 
@@ -66,25 +69,59 @@ const fecthApi = (option, key) => {
                 .then(data => {
                     showDocument(data);
                 })
-                .catch(err => {
-                    // showErrorDocument();
-                });
             break;
 
         case 'bycategory':
 
-            fetch(`http://localhost:5000/products/bycategory?idCategory=${key}`, options)
+            fetch(`http://localhost:5000/products/bycategory?idCategory=${second_key}&name=${key}`, options)
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data)
+                    showDocument(data);
                 })
-                .catch(err => {
-                    console.log(err);
-                });
             break;
     }
 }
 
-// getAll();
-document.getElementById('form').addEventListener('submit', getByName);
+
+const getAllProducts = () => {
+    productView.innerHTML = `WAIT A MINUTE...!`;
+    fecthApi('allProducts', '', '');
+}
+
+const getByName = (e) => {
+    e.preventDefault();
+    productView.innerHTML = `WAIT A MINUTE...!`;
+    const key = document.getElementById('name').value;
+
+    fecthApi('byName', key, '');
+}
+
+const getAllCategories = () => {
+    const key = document.getElementById('name').value;
+    fecthApi('categories', key, '');
+}
+
+const getByCategory = (e) => {
+    e.preventDefault();
+    productView.innerHTML = `WAIT A MINUTE...!`;
+    const key = document.getElementById('name').value;
+    fecthApi('bycategory', key, category);
+}
+
+categoryView.addEventListener('change', (e) => {
+    category = categoryView.value;
+});
+
+const start = (e) => {
+    e.preventDefault();
+
+    category < 0
+        ?getByName(e)
+        :getByCategory(e);
+}
+
+getAllCategories();
+getAllProducts();
+form.addEventListener('submit', start);
+
 
